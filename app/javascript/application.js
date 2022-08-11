3c2a89d650131df8d3942de2f3cfaf07b130193f
timjini <hatim.jini@gmail.com>
2022-08-11 22:17:02 +0300

    initial

---
+++ app/javascript/application.js
@@ -0,0 +1,23 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import "trix"
+import "@rails/actiontext"
+
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '10';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
