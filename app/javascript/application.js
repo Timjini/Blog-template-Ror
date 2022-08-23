bbb90a418936a9157a4f07dc79c9b4886a873209
timjini <hatim.jini@gmail.com>
2022-08-23 17:25:38 +0300

    navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,21 +3,3 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
-
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
-
-
-
-
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
-
-function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '10';
-}
-function close(){
-    mainMenu.style.top = '-100%';
-}
