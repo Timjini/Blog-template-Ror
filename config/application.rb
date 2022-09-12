6b07dad02c9c86518116fc1aeb20489614fd3c11
timjini <hatim.jini@gmail.com>
2022-09-12 14:31:52 +0300

    update

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module FashionBlog
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.active_storage.variant_processor = :mini_magick
+    config.serve_static_files = true 
 
   end
 end
