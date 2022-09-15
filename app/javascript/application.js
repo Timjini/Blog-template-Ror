79bc2d203e3924385cf361e7bef0de6ad8a1971b
timjini <hatim.jini@gmail.com>
2022-09-16 00:39:00 +0300

    no signup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,50 +4,57 @@ import "controllers"
 import "trix"
 import "@rails/actiontext"
 
-// Dealing with Input width
-let el = document.querySelector(".input-wrap .input");
-let widthMachine = document.querySelector(".input-wrap .width-machine");
-el.addEventListener("keyup", () => {
-  widthMachine.innerHTML = el.value;
-});
-
-// Dealing with Textarea Height
-function calcHeight(value) {
-  let numberOfLineBreaks = (value.match(/\n/g) || []).length;
-  // min-height + lines x line-height + padding + border
-  let newHeight = 20 + numberOfLineBreaks * 20 + 12 + 2;
-  return newHeight;
-}
-
-let textarea = document.querySelector(".resize-ta");
-textarea.addEventListener("keyup", () => {
-  textarea.style.height = calcHeight(textarea.value) + "px";
-});
 
 
-//Navbar hamburger button
 
-const button = document.querySelector('#menu-button');
-const menu = document.querySelector('#menu');
+// //Navbar hamburger button
 
-
-button.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
-});
+// const button = document.querySelector('#menu-button');
+// const menu = document.querySelector('#menu');
 
 
-//CARDS ANIMATION
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-      console.log(entry)
-      if (entry.isIntersecting){
-          entry.target.classList.add('show');
-      } else {
-          entry.target.classList.remove('show');
-      }
-  }); 
-});
+// button.addEventListener('click', () => {
+//   menu.classList.toggle('hidden');
+// });
 
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+// //CARDS ANIMATION
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//       console.log(entry)
+//       if (entry.isIntersecting){
+//           entry.target.classList.add('show');
+//       } else {
+//           entry.target.classList.remove('show');
+//       }
+//   }); 
+// });
+
+
+// const hiddenElements = document.querySelectorAll('.hidden');
+// hiddenElements.forEach((el) => observer.observe(el));
+
+
+//Make textarea expendable
+// function getScrollHeight(elm){
+//   var savedValue = elm.value
+//   elm.value = ''
+//   elm._baseScrollHeight = elm.scrollHeight
+//   elm.value = savedValue
+// }
+
+// function onExpandableTextareaInput({ target:elm }){
+//   // make sure the input event originated from a textarea and it's desired to be auto-expandable
+//   if( !elm.classList.contains('autoExpand') || !elm.nodeName == 'trix-editor' ) return
+  
+//   var minRows = elm.getAttribute('data-min-rows')|0, rows;
+//   !elm._baseScrollHeight && getScrollHeight(elm)
+
+//   elm.rows = minRows
+//   rows = Math.ceil((elm.scrollHeight - elm._baseScrollHeight) / 16)
+//   elm.rows = minRows + rows
+// }
+
+
+// // global delegated event listener
+// document.addEventListener('input', onExpandableTextareaInput)
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,17 @@ Rails.application.routes.draw do
   
   root 'home#index'  
 
-  get 'pages/about'
+  match '/about',  to: 'home#about',via: 'get'
+  match '/contact',  to: 'home#contact',via: 'get'
+  match '/shop-the-look',  to: 'home#shop-the-look',via: 'get'
 
   get 'pages/home'
-  devise_for :users
+  devise_for :users #,:skip => [:registrations] 
   resources :posts
+
+  devise_scope :user do
+    get "login" => "devise/sessions#new"
+    end
+
 
 end
