fdb1b45983c90384930d40d92f935f579ab1e517
timjini <hatim.jini@gmail.com>
2022-09-29 13:37:38 +0300

    error page

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "login" => "devise/sessions#new"
     end
-
+    match "/404", to: "errors#not_found", via: :all
+    match "/500", to: "errors#internal_server_error", via: :all
 
 end
